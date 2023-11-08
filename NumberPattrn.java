776b11c32fbef5118d85eb6910023b58bca31f93
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-11-08 22:53:40 +0530

    Add files via upload

---
+++ NumberPattrn.java
@@ -0,0 +1,12 @@
+class NumberPattrn{
+ public static void main(String[] args){
+  int n = 1;
+  for(int i = 1;i<5;i++){
+   for(int j = 0;j<i;j++){
+    System.out.print(n+" " );
+    n=n+1;
+   }
+   System.out.println("");
+  }
+ }
+}
