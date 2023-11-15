7fce41c0feda3cffd0d855db9e581d19621520e4
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-11-15 19:21:28 +0530

    Add files via upload

---
+++ IfElseStatement.java
@@ -0,0 +1,12 @@
+class IfElseStatement {
+ public static void main(String[] args){
+  
+  int age = 13;
+
+  if(age >= 18){
+   System.out.println("You can drive the car");
+  } else {
+   System.out.println("You need to be over 18 years old to drive the car");
+  }
+ }
+}
