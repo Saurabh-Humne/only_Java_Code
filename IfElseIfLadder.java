5a2bfd72a58618e78dfd24fa2bcb1f48d9089851
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-11-15 21:42:01 +0530

    Add files via upload

---
+++ IfElseIfLadder.java
@@ -0,0 +1,17 @@
+class IfElseIfLadder {
+ public static void main(String[] args){
+  int age = 40;
+
+  if(age <= 3){
+   System.out.println("You are a todler");
+  } else if(age <= 14){
+   System.out.println("Yor are a kid");
+  } else if(age <= 30 ){
+   System.out.println("You are a adult");
+  } else {
+   System.out.println("You are Boomer");
+  }
+
+
+ }
+}
