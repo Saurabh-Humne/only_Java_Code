94880b23e9ba819f7890c2b944369dc0e825a965
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-11-16 22:00:37 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,10 @@
+class ForLoop {
+ public static void main(String[] args){
+  int num = 9;
+  System.out.println("Print the table of:"+num);
+
+  for(int i=1;i<=10;i++){
+   System.out.println(num*i);
+  }
+ }
+}
