685bf3e39757ecd258c4d9bbc99b49f592d5560f
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-11-16 22:10:44 +0530

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,10 @@
+class ForEachLoop {
+ public static void main(String[] args){
+  String[] name = {"Circle","Square","Rectangle","Tringle"};
+  System.out.println("Printing the String data :");
+
+  for(String data:name){
+   System.out.println(data);
+  }
+ }
+}
