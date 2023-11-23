336209a74fac418296821660b008a685fb247c60
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-11-23 20:43:01 +0530

    Add files via upload

---
+++ MethodParameters.java
@@ -0,0 +1,9 @@
+class MethodParameters {
+ static void newMethod(String name, int age){
+  System.out.println(name + " " + age +" "+ "form Method");
+ }
+
+ public static void main(String[] args){
+  newMethod("leo", 20);
+ }
+}
