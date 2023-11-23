033627f8950cafd427d8e0a1aa3f1c93b7516018
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-11-23 15:21:40 +0530

    Add files via upload

---
+++ ForeachInArray.java
@@ -0,0 +1,11 @@
+// for (type varible : arrayname) {../code/..}
+
+class ForeachInArray {
+ public static void main(String[] args){
+  String[] food = {"Burger", "Pizza","Hotdog","Sanwitch"};
+
+  for (String i : food){
+   System.out.println(i);
+  }
+ }
+}
