eca9ec45ad5fe8ebbea1e9d9d1bb1417a3fd5077
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-11-27 21:14:08 +0530

    Add files via upload

---
+++ ClassAndObject.java
@@ -0,0 +1,11 @@
+class ClassAndObject {
+ String note = "Hello, World";
+
+ public static void main(String[] args){
+  ClassAndObject obj1 = new ClassAndObject();
+  ClassAndObject obj2 = new ClassAndObject();
+  System.out.println(obj1.note);
+  System.out.println(obj2.note);
+
+ }
+}
