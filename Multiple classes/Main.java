5188d89eb528633abc36930d1677d0741f0a6d6b
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-03 13:37:09 +0530

    Add files via upload

---
+++ Multiple classes/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+ public void dog(String name) {
+  System.out.println("My Dog name is :"+ name);
+ }
+
+ public void age(int age){
+  System.out.println("his age is :" +age + " Years Old");
+ }
+}
---
+++ Multiple classes/Pro.java
@@ -0,0 +1,7 @@
+class Pro{
+ public static void main(String[] args){
+  Main obj = new Main();
+  obj.dog("Toney");
+  obj.age(4);
+ }
+}
