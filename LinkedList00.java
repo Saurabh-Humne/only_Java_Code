d4111795ff3efc17fa2c6ec01afb302b1cf4bd95
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-08 17:11:14 +0530

    Add files via upload

---
+++ LinkedList00.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+class LinkedList00 {
+ public static void main(String[] args){
+  LinkedList<String> fruts = new LinkedList<String>();
+  fruts.add("Apple");
+  fruts.add("Banana");
+  fruts.add("Mango");
+  fruts.add("Tamato");
+
+
+  System.out.println(fruts);
+ }
+}
