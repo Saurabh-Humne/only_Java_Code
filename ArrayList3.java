c2dee7f1f0f8734c315464d11880d8c4850459f1
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-08 15:46:21 +0530

    Create ArrayList3.java

---
+++ ArrayList3.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class ArrayList3 {
+ public static void main(String[] args){
+  ArrayList<String> fruts = new ArrayList<String>();
+  fruts.add("Apple");
+  fruts.add("Orange");
+  fruts.add("Tomato");
+  fruts.add("Jackfrut");
+  
+  for (String i : fruts){
+   System.out.println(i);
+  }
+ }
+}
