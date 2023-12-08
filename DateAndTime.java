2c32257b3bdf9244d37f8a1d0c82fae8f99b482c
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-08 14:22:50 +0530

    Add files via upload

---
+++ DateAndTime.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+class DateAndTime{
+ public static void main(String[] args){
+  LocalDateTime myDateTime = LocalDateTime.now();
+  System.out.println(myDateTime);
+ }
+}
