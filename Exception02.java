f16a16c490b09fffff8ee9c09bb43ea8ad40b528
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-10 20:05:15 +0530

    Add files via upload

---
+++ Exception02.java
@@ -0,0 +1,15 @@
+public class Exception02 {
+ static void checkAge(int age) {
+  if (age < 18) {
+   throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+   //System.out.println("nono");
+  }
+  else {
+   System.out.println("Access Granted");
+  }
+ }
+
+ public static void main(String[] args){
+  checkAge(15);
+ }
+}
