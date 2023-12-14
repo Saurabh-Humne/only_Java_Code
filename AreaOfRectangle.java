51adb5db74e9c39b206b1bbe116f84b88d16482d
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-14 20:44:28 +0530

    Create AreaOfRectangle.java

---
+++ AreaOfRectangle.java
@@ -0,0 +1,10 @@
+class AreaOfRectangle {
+ static int area(int length, int breadth){
+  return length * breadth;
+ }
+
+  public static void main(String[] args){
+   int rectangle = area(8,4);
+   System.out.println(rectangle);
+  }
+}
