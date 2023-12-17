ac2a35362142060e23bbe51039d4e2fca83f313a
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-17 18:09:12 +0530

    Create ClassEx01.java

---
+++ ClassEx01.java
@@ -0,0 +1,16 @@
+class Student {
+ int id;
+ String name;
+}
+
+class ClassEx01 {
+ public static void main(String[] args){
+
+  Student s1 = new Student();
+
+  System.out.println(s1.id);
+  System.out.println(s1.name);
+ 
+ }
+
+}
