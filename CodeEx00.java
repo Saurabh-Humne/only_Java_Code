147ccf52d6dd1ba9066ee85c72b846cd1dbb5e14
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-17 18:03:22 +0530

    Create CodeEx00.java

---
+++ CodeEx00.java
@@ -0,0 +1,14 @@
+class ClassEx00 {
+ int id;
+ String name;
+
+ public static void main(String[] args){
+  
+  ClassEx00 c1 = new ClassEx00();
+
+  System.out.println(c1.id);
+  System.out.println(c1.name);
+
+ }
+
+}
