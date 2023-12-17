b01b8c9fcb2732ea59bdab1e0be01d105b494fa3
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-17 18:15:29 +0530

    Create ClassEx02.java

---
+++ ClassEx02.java
@@ -0,0 +1,20 @@
+class Student {
+ 
+ int id;
+ String name;
+
+}
+
+class ClassEx02 {
+ public static void main(String[] args){
+  
+  Student s1 = new Student();
+
+   s1.id = 57;
+   s1.name = "King Kong";
+
+   System.out.println(s1.id + " " + s1.name);
+
+ }
+
+}
