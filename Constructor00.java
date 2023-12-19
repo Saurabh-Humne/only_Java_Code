67ec7dc330439eeda409a6922a8a9ecd11846e5c
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-19 21:03:39 +0530

    Create Constructor00.java

---
+++ Constructor00.java
@@ -0,0 +1,10 @@
+class Constructor00 {
+ Constructor00() {
+  System.out.println("Construcor is creaed");
+ }
+
+ public static void main(String[] args){
+  
+  Constructor00 con = new Constructor00();
+ }
+}
