dbeb76ba122c2526eb209b6168f500e911eaccbd
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2023-12-29 23:17:00 +0530

    Create J04Static.java

---
+++ J04Static.java
@@ -0,0 +1,10 @@
+class J04Static {
+ static int cube(int x){
+  return x*x*x;
+ }
+
+ public static void main(String[] args){
+  int result = J04Static.cube(5);
+  System.out.println(result);
+ }
+}
