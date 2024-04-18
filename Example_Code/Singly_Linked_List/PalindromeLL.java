d2cdc16e56e412558674f4e25c41d8e4ed42d4f6
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2024-04-18 08:19:57 +0530

    Create PalindromeLL.java

---
+++ Example_Code/Singly_Linked_List/PalindromeLL.java
@@ -0,0 +1,102 @@
+
+public class PalindromeLL {
+
+	class Node{
+		int data;
+		Node next;
+	
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	public int size;
+	
+	public Node head = null;
+	public Node tail = null;
+	
+	public void addNode(int data) {
+		Node newNode = new Node(data);
+		
+		if(head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+		size++;
+	}
+	
+	public Node reverseList(Node temp) {
+		Node current = temp;
+		Node prevNode = null, nextNode = null;
+		
+		while(current != null) {
+			nextNode = current.next;
+			current.next = prevNode;
+			prevNode = current;
+			current = nextNode;
+		}
+		return prevNode;
+	}
+	
+	public void isPalindromeLL() {
+		Node current = head;
+		boolean flag= true;
+		
+		int mid = (size%2 == 0)? (size/2):((size+1)/2);
+		
+		for(int i =1; i<mid; i++) {
+			current = current.next;
+		}
+		
+		Node revHead = reverseList(current.next);
+		
+		while(head != null && revHead != null) {
+			if(head.data != revHead.data) {
+				flag = false;
+				break;
+			}
+			head = head.next;
+			revHead = revHead.next;
+		}
+		
+		if(flag) {
+			System.out.println("Give singly linked list is palindome");
+		} else {
+			System.out.println("Give singly linked list is not a palindrome");
+		}
+	
+	}
+	
+	public void display() {
+		Node current = head;
+		
+		if(head == null) {
+			System.out.println("List is empty");
+			return;
+		}
+		System.out.println("Node of isngly linked list: ");
+		while(current != null) {
+			System.out.print(current.data + " ");
+			current = current.next;
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		PalindromeLL sList = new PalindromeLL();
+		
+		sList.addNode(1);
+		sList.addNode(2);
+		sList.addNode(3);
+		sList.addNode(2);
+		sList.addNode(1);
+		
+		sList.display();
+		
+		sList.isPalindromeLL();
+	}
+}
