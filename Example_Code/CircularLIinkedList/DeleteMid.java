0a863d9460329b44e2977059fad7ce480327658d
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2024-04-30 23:48:39 +0530

    Create DeleteMid.java

---
+++ Example_Code/CircularLIinkedList/DeleteMid.java
@@ -0,0 +1,94 @@
+
+public class DeleteMid {
+
+	public class Node{
+		int data;
+		Node next;
+		public Node(int data) {
+			this.data = data;
+		}
+	}
+	
+	public int size;
+	
+	public Node head = null;
+	public Node tail = null;
+	
+	public void add(int data) {
+		Node newNode = new Node(data);
+		if(head == null) {
+			
+			head = newNode;
+			tail = newNode;
+			newNode.next = head;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+			tail.next = head;
+		}
+		size++;
+	}
+	
+	public void deleteMid() {
+		Node current, temp;
+		
+		if(head == null) {
+			return;
+		} else {
+			int count = (size %2 == 0)? (size/2):((size+1)/2);
+			
+			if(head != tail) {
+				temp = head;
+				current = null;
+				
+				for(int i = 0; i< count-1; i++) {
+					current = temp;
+					temp = temp.next;
+				}
+				
+				if(current != null) {
+					current.next = temp.next;
+					temp = null;
+				} else {
+					head = tail = temp.next;
+					tail.next = head;
+					temp = null;
+				}
+
+			} else {
+				head = tail = null;
+			}
+		}
+		size--;
+	}
+	
+	public void display() {
+		Node current = head;
+		if(head == null) {
+			System.out.println("List is empty");
+		} else {
+			do {
+				System.out.print(" " + current.data);
+				current = current.next;
+			} while (current != head);
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		DeleteMid cl = new DeleteMid();
+		
+		cl.add(1);
+		cl.add(2);
+		cl.add(3);
+		cl.add(4);
+		
+		System.out.println(" Original List: ");
+		cl.display();
+		while(cl.head != null) {
+			cl.deleteMid();
+			System.out.println("Updated List: ");
+			cl.display();
+		}
+	}
+}
