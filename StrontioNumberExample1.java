6892da101ec8963f6a9b03ea2f6aa360e5b311e5
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2024-07-12 23:29:43 +0530

    Create StrontioNumberExample1.java

---
+++ StrontioNumberExample1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class StrontioNumberExample1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		//read an integer from the user
+		int num = sc.nextInt();
+		int n = num;
+		//first, we hava multipplied a number by 2
+		//the result is divided by 1000 that gives the remainder and remove the first digit
+		//at last, the result is divided by 10 that remove the last digit
+		//therefore, we get a two-digit number that are mean digit of the given number
+		num = (num*2%1000)/10;
+		//divide the two-digit number (that we get from the above) by 10 and find the remainder
+		//compares the remainder and quotient
+		if(num%10==num/10)
+			//if equal, prints strontio number
+			System.out.println(n + "is a strontio number.");
+		else
+			//print if not a strontio number
+			System.out.println(n + " is not a strontio number.");
+	}
+}
