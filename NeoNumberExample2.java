cbf76c225f41dc6471a834343d4f31eacf856507
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2024-07-18 23:12:08 +0530

    Create NeoNumberExample2.java

---
+++ NeoNumberExample2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class NeoNumberExample2 {
+
+	static boolean isNeon(int num) {
+		int sq = num * num;
+		int sumOfdigits = 0;
+		
+		while (sq != 0) {
+			sumOfdigits = sumOfdigits + sq % 10;
+			sq = sq/ 10;
+			
+		}
+		
+		return (sumOfdigits == num);
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Neon Numbers between the given range are: ");
+		
+		for (int i = 0; i <= 100000; i++)
+			if(isNeon(i))
+				System.out.print(i+ " ");
+	}
+}
