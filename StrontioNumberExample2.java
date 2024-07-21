ab69fa89cbea74098eeb57d04bf1a2742882e16b
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2024-07-21 23:00:05 +0530

    Create StrontioNumberExample2.java

---
+++ StrontioNumberExample2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class StrontioNumberExample2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a four- digit number.");
+		
+		int num = sc.nextInt();
+		
+		if((num>999)&&(num<10000)) {
+			int n = num;
+			num = num/10;
+			num = num%100;
+			
+			if((num/10)==(num%10))
+				System.out.println(n+" is a strontio number.");
+			else
+				System.out.println(n+ " is not a strong number.");
+		} else
+			System.out.println("Please enter a four-digit number.");
+	}
+}
